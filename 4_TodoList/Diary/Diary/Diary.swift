cb29b228493800e1b833ef1f32d17085981c8fc0
Donguk Lee <uniqueimaginate@gmail.com>
2022-02-11 17:45:52 +0900

    Basic CH05 - Diary write, delete, edit

---
+++ 4_TodoList/Diary/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Peter on 2022/02/10.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
