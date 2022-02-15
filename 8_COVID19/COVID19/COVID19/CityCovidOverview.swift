516b438b4db149d783a8f906e4c145c7f8268476
Donguk Lee <uniqueimaginate@gmail.com>
2022-02-15 22:56:24 +0900

    Basic CH08 - Completed COVID19 app

---
+++ 8_COVID19/COVID19/COVID19/CityCovidOverview.swift
@@ -0,0 +1,40 @@
+//
+//  CityCovidOverview.swift
+//  COVID19
+//
+//  Created by Peter on 2022/02/15.
+//
+
+import Foundation
+
+struct CityCovidOverview: Codable {
+    let korea: CovidOverView
+    let seoul: CovidOverView
+    let busan: CovidOverView
+    let daegu: CovidOverView
+    let incheon: CovidOverView
+    let gwangju: CovidOverView
+    let daejeon: CovidOverView
+    let ulsan: CovidOverView
+    let sejong: CovidOverView
+    let gyeonggi: CovidOverView
+    let gangwon: CovidOverView
+    let chungbuk: CovidOverView
+    let chungnam: CovidOverView
+    let jeonbuk: CovidOverView
+    let jeonnam: CovidOverView
+    let gyeongbuk: CovidOverView
+    let gyeongnam: CovidOverView
+    let jeju: CovidOverView
+}
+
+struct CovidOverView: Codable {
+    let countryName: String
+    let newCase: String
+    let totalCase: String
+    let recovered: String
+    let death: String
+    let percentage: String
+    let newCcase: String
+    let newFcase: String
+}
