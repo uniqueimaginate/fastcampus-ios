61eddb634e19e90d108ea36f82763b6779fa9d55
Donguk Lee <uniqueimaginate@gmail.com>
2022-03-22 00:00:04 +0900

    Intermediate CH04 - Drink - Add UNNotification Trigger

--- 12_Drink/12_Drink/UNNotificationCenter.swift
+++ 12_Drink/12_Drink/UNNotificationCenter.swift
@@ -15,5 +15,9 @@ extension UNUserNotificationCenter {
         content.body = "세계보건기구(WHO)가 권장하는 하루 물 섭취량은 1.5~2.0리터 입니다."
         content.sound = .default
         content.badge = 1
+        
+        let component = Calendar.current.dateComponents([.hour, .minute], from: alert.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: alert.isOn)
     }
+    
 }
